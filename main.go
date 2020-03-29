6bf77540811a2f8f263d6c444121561742f2f2fa
DQNEO <dqneoo@gmail.com>
2020-03-29 23:26:15 +0900

    Impl binary add operation

--- main.go
+++ main.go
@@ -10,18 +10,32 @@ import (
 func emitExpr(expr ast.Expr) {
 	switch e := expr.(type) {
 	case *ast.BasicLit:
+		fmt.Printf("# start %T\n", e)
 		val := e.Value
 		ival, _ := strconv.Atoi(val)
-		fmt.Printf("# %T\n", expr)
 		fmt.Printf("  movq $%d, %%rax\n", ival)
 		fmt.Printf("  pushq %%rax\n")
+		fmt.Printf("# end %T\n", e)
+	case *ast.BinaryExpr:
+		fmt.Printf("# start %T\n", e)
+		emitExpr(e.X)
+		emitExpr(e.Y)
+		if e.Op.String() == "+" {
+			fmt.Printf("  popq %%rax\n")
+			fmt.Printf("  popq %%rdi\n")
+			fmt.Printf("  addq %%rdi, %%rax\n")
+			fmt.Printf("  pushq %%rax\n")
+		} else {
+			panic(fmt.Sprintf("Unexpected binary operator %s", e.Op))
+		}
+		fmt.Printf("# end %T\n", e)
 	default:
 		panic(fmt.Sprintf("Unexpected expr type %T", expr))
 	}
 }
 
 func main() {
-	source := "42"
+	source := "40 + 2"
 	expr, err := parser.ParseExpr(source)
 	if err != nil {
 		panic(err)
