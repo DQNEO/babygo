a31180e8865ccf6f0d0d2c2702b48698bbf0c47f
DQNEO <dqneoo@gmail.com>
2020-03-30 21:36:59 +0900

    Add var and var

--- t/source.go
+++ t/source.go
@@ -2,11 +2,12 @@ package main
 
 import "os"
 
-var myInt int = 40
+var myInt int = 30
+var myInt2 int = 12
 var myString string = "to stderr\n"
 
 func main() {
 	print("hello world\n")
 	print(myString)
-	os.Exit(myInt + 2)
+	os.Exit(myInt + myInt2)
 }
