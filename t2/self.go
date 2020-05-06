b888405822bf6ec7866c082835795bb21cad4154
DQNEO <dqneoo@gmail.com>
2020-05-06 11:30:32 +0900

    Extract fake func in self.go

--- t2/self.go
+++ t2/self.go
@@ -91,47 +91,7 @@ func emitPopBool(comment string) {
 
 
 func semanticAnalyze(name string) string {
-	globalFuncs = make([]*Func, 2, 2)
-	var fnc *Func = new(Func)
-	fnc.name = "main"
-	globalFuncs[0] = fnc
-	var fnc2 *Func = new(Func)
-	fnc2.name = "foo"
-	globalFuncs[1] = fnc2
-
-	stringLiterals = make([]*sliteral, 2, 2)
-	var s1 *sliteral = new(sliteral)
-	s1.value = "hello0"
-	s1.label = ".main.S0"
-	stringLiterals[0] = s1
-
-	var s2 *sliteral = new(sliteral)
-	s2.value = "hello1"
-	s2.label = ".main.S1"
-	stringLiterals[1] = s2
-
-	var globalVar0 *astValueSpec = new(astValueSpec)
-	globalVar0.name = "_gvar0"
-	globalVar0.value = "10"
-	globalVar0.t = new(Type)
-	globalVar0.t.kind = "T_INT"
-	globalVars = append(globalVars, globalVar0)
-
-	var globalVar1 *astValueSpec = new(astValueSpec)
-	globalVar1.name = "_gvar1"
-	globalVar1.value = "11"
-	globalVar1.t = new(Type)
-	globalVar1.t.kind = "T_INT"
-	globalVars = append(globalVars, globalVar1)
-
-	var globalVar2 *astValueSpec = new(astValueSpec)
-	globalVar2.name = "_gvar2"
-	globalVar2.value = "foo"
-	globalVar2.t = new(Type)
-	globalVar2.t.kind = "T_STRING"
-	globalVars = append(globalVars, globalVar2)
-
-	return name
+	return fakeSemanticAnalyze(name)
 }
 
 const T_STRING string = "T_STRING"
@@ -252,3 +212,48 @@ func main() {
 		generateCode(pkgName)
 	}
 }
+
+func fakeSemanticAnalyze(name string) string {
+	globalFuncs = make([]*Func, 2, 2)
+	var fnc *Func = new(Func)
+	fnc.name = "main"
+	globalFuncs[0] = fnc
+	var fnc2 *Func = new(Func)
+	fnc2.name = "foo"
+	globalFuncs[1] = fnc2
+
+	stringLiterals = make([]*sliteral, 2, 2)
+	var s1 *sliteral = new(sliteral)
+	s1.value = "hello0"
+	s1.label = ".main.S0"
+	stringLiterals[0] = s1
+
+	var s2 *sliteral = new(sliteral)
+	s2.value = "hello1"
+	s2.label = ".main.S1"
+	stringLiterals[1] = s2
+
+	var globalVar0 *astValueSpec = new(astValueSpec)
+	globalVar0.name = "_gvar0"
+	globalVar0.value = "10"
+	globalVar0.t = new(Type)
+	globalVar0.t.kind = "T_INT"
+	globalVars = append(globalVars, globalVar0)
+
+	var globalVar1 *astValueSpec = new(astValueSpec)
+	globalVar1.name = "_gvar1"
+	globalVar1.value = "11"
+	globalVar1.t = new(Type)
+	globalVar1.t.kind = "T_INT"
+	globalVars = append(globalVars, globalVar1)
+
+	var globalVar2 *astValueSpec = new(astValueSpec)
+	globalVar2.name = "_gvar2"
+	globalVar2.value = "foo"
+	globalVar2.t = new(Type)
+	globalVar2.t.kind = "T_STRING"
+	globalVars = append(globalVars, globalVar2)
+
+	return name
+}
+
