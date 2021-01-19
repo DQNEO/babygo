0783b46bcc7f7de0d5cbf7a2bd441872f7c18fc8
DQNEO <dqneoo@gmail.com>
2021-01-19 17:18:37 +0900

    Improve error output

--- test.sh
+++ test.sh
@@ -3,15 +3,13 @@ set -u
 program=$1
 ${program} myargs 1>/tmp/actual.1 2> /tmp/actual.2
 exit_status=$?
-diff t/expected.txt /tmp/actual.1
-if [[ $? -ne 0 ]]; then
-  echo "stdout differs"
-  exit 1
-fi
 
 if [[ $exit_status -eq 0 ]]; then
   echo ok
 else
   echo FAILED
+  echo
+  echo "    ${program} myargs 1>/tmp/actual.1 2> /tmp/actual.2"
+  echo
   exit $exit_status
 fi
