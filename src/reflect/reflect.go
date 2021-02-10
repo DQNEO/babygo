4824c9fa58dd1dbd705c3b57e8870a0c3d956d0a
DQNEO <dqneoo@gmail.com>
2021-02-10 13:15:57 +0900

    Refactor

--- src/reflect/reflect.go
+++ src/reflect/reflect.go
@@ -23,16 +23,9 @@ type _type struct {
 }
 
 func (t *Type) String() string {
-	switch t.X.(type) {
-	//case int:
-	//	return "int"
-	//case string:
-	//	return "string"
-	default:
-		ifc := t.X
-		var ifcAddr uintptr = uintptr(unsafe.Pointer(&ifc))
-		var pEface *eface = (*eface)(unsafe.Pointer(ifcAddr))
-		typ := pEface._type
-		return typ.name
-	}
+	ifc := t.X
+	var ifcAddr uintptr = uintptr(unsafe.Pointer(&ifc))
+	var pEface *eface = (*eface)(unsafe.Pointer(ifcAddr))
+	typ := pEface._type
+	return typ.name
 }
