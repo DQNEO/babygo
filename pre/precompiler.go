b1b1ed8a22768628d5eed2e729c4ea75f78c40ec
DQNEO <dqneoo@gmail.com>
2021-02-15 10:16:04 +0900

    tmp

--- pre/precompiler.go
+++ pre/precompiler.go
@@ -574,7 +574,11 @@ func emitCall(symbol string, args []*Arg, results []*ast.Field) {
 		arg.offset = totalParamSize
 		totalParamSize += getSizeOfType(arg.paramType)
 	}
-	emitAllocReturnVarsArea(sliceSize)
+	var totalReturnSize int
+	for _, r := range results {
+		totalReturnSize += getSizeOfType(e2t(r.Type))
+	}
+	emitAllocReturnVarsArea(totalReturnSize)
 	fmt.Printf("  subq $%d, %%rsp # alloc parameters area\n", totalParamSize)
 	for _, arg := range args {
 		paramType := arg.paramType
@@ -590,7 +594,8 @@ func emitCall(symbol string, args []*Arg, results []*ast.Field) {
 
 	fmt.Printf("  callq %s\n", symbol)
 	emitFreeParametersArea(totalParamSize)
-	emitFreeReturnVarsArea(sliceSize)
+	fmt.Printf("#  totalReturnSize=%d\n", totalReturnSize)
+	//emitFreeReturnVarsArea(totalReturnSize)
 	emitReturnedValue(results)
 }
 
