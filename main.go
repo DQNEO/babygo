ccf0f86db87a5a842dcb4c5565cc7d3f18e6e9eb
DQNEO <dqneoo@gmail.com>
2021-02-27 18:47:21 +0900

    Inject meta node from outside

--- main.go
+++ main.go
@@ -4009,3 +4009,48 @@ type MetaTypeSwitchCaseClose struct {
 	VariableType *Type
 	Orig         *ast.CaseClause
 }
+
+
+type Type struct {
+	//kind string
+	E ast.Expr
+}
+
+type Variable struct {
+	Name         string
+	IsGlobal     bool
+	GlobalSymbol string
+	LocalOffset  int
+	Typ          *Type
+}
+
+type Func struct {
+	Localarea int
+	Argsarea  int
+	LocalVars []*Variable
+	Params    []*Variable
+	Retvars   []*Variable
+	FuncType  *ast.FuncType
+	RcvType   ast.Expr
+	Name      string
+	Stmts     []ast.Stmt
+	Method    *Method
+}
+
+
+type Method struct {
+	PkgName      string
+	RcvNamedType *ast.Ident
+	IsPtrMethod  bool
+	Name         string
+	FuncType     *ast.FuncType
+}
+
+type MetaForStmt struct {
+	LabelPost   string
+	LabelExit   string
+	RngLenvar   *Variable
+	RngIndexvar *Variable
+	Outer       *MetaForStmt
+}
+
