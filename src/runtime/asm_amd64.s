68908c441cb7e92e0b5386eecc95f0be19319bc4
DQNEO <dqneoo@gmail.com>
2021-09-14 23:32:54 +0900

    Be more like go runtime

--- src/runtime/asm_amd64.s
+++ src/runtime/asm_amd64.s
@@ -1,3 +1,7 @@
+.data
+runtime.main_main:
+  .quad 0
+
 .text
 
 runtime.rt0_go:
@@ -22,6 +26,10 @@ runtime.rt0_go:
   addq $16,  %rax # + 16 (skip null and go to next) => envp
   movq %rax, runtime.envp+0(%rip) # envp
 
+  # register main_main
+  leaq main.main(%rip), %rax
+  movq %rax, runtime.main_main(%rip)
+
   callq runtime.heapInit
 
   callq runtime.__initGlobals
@@ -30,10 +38,14 @@ runtime.rt0_go:
   callq main.__initGlobals
 
   callq os.init # set os.Args
-  callq main.main
 
+  movq runtime.main_main(%rip), %rax
+  callq *%rax
+
+  callq runtime.exit
+  # End of program
+
+runtime.exit:
   movq $0, %rdi  # status 0
   movq $60, %rax # sys_exit
   syscall
-  # End of program
-
