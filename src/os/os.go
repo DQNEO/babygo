04329683fda7e2c3af12708cddb23bb920f97dfc
DQNEO <dqneoo@gmail.com>
2021-09-19 21:44:48 +0900

    Move Readdirnames to os package

--- src/os/os.go
+++ src/os/os.go
@@ -1,6 +1,7 @@
 package os
 
 import "syscall"
+import "unsafe"
 
 const SYS_EXIT int = 60
 var Args []string
@@ -43,6 +44,71 @@ func Create(name string) (*File, error) {
 
 func (f *File) Fd() uintptr {
 	return uintptr(f.fd)
+}
+
+func Cstring2string(b *byte) string {
+	var bs []byte
+	for {
+		if b == nil || *b == 0 {
+			break
+		}
+		bs = append(bs, *b)
+		p := uintptr(unsafe.Pointer(b)) + 1
+		b = (*byte)(unsafe.Pointer(p))
+	}
+	return string(bs)
+}
+
+// Translation of http://man7.org/linux/man-pages/man2/getdents64.2.html#top_of_page
+
+//struct linux_dirent64 {
+//	ino64_t        d_ino;    // 8 bytes: 64-bit inode number
+//	off64_t        d_off;    // 8 bytes: 64-bit offset to next structure
+//	unsigned short d_reclen; // 2 bytes: Size of this dirent
+//	unsigned char  d_type;   // 1 byte: File type
+//	char           d_name[]; // Filename (null-terminated)
+//};
+
+type linux_dirent struct {
+	d_ino     int
+	d_off     int
+	d_reclen1 uint16
+	d_type    byte
+	d_name    byte
+}
+
+func (f *File) Readdirnames(n int) ([]string, error) {
+	var fd uintptr = f.Fd()
+	var buf []byte = make([]byte, 1024, 1024)
+	var counter int
+	var entries []string
+	for {
+		nread, _ := syscall.Getdents(int(fd), buf)
+		if nread == -1 {
+			panic("getdents failed")
+		}
+		if nread == 0 {
+			break
+		}
+
+		var bpos int
+		for bpos < nread {
+			var dirp *linux_dirent
+			p := uintptr(unsafe.Pointer(&buf[0])) + uintptr(bpos)
+			dirp = (*linux_dirent)(unsafe.Pointer(p))
+			pp := unsafe.Pointer(&dirp.d_name)
+			var bp *byte = (*byte)(pp)
+			var s string = Cstring2string(bp)
+			bpos = bpos + int(dirp.d_reclen1)
+			counter++
+			if s == "." || s == ".." {
+				continue
+			}
+			entries = append(entries, s)
+		}
+	}
+	f.Close()
+	return entries, nil
 }
 
 func (f *File) Close() error {
