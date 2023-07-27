e937f2987ef74ab1702c3ea127fccf99472c2f54
DQNEO <dqneoo@gmail.com>
2023-07-27 20:53:18 +0900

    Increase heap for now

--- src/runtime/runtime.go
+++ src/runtime/runtime.go
@@ -117,7 +117,7 @@ var Envs []*envEntry
 
 func heapInit() {
 	heapHead = brk(0)
-	heapTail = brk(heapHead + heapSize*2)
+	heapTail = brk(heapHead + heapSize*16)
 	if heapHead%8 == 0 {
 		heapCurrent = heapHead
 	} else {
