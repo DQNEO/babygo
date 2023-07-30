cada8d1232fcf92a20dc8b6a577c18ce65d3e69a
DQNEO <dqneoo@gmail.com>
2023-07-30 13:38:41 +0900

    Move syscal.Syscall from runtime to syscall package

--- src/runtime/runtime.s
+++ src/runtime/runtime.s
@@ -96,14 +96,3 @@ runtime.Syscall:
   movq %rax, 40(%rsp) # r0 uintptr
   ret
 
-// func Syscall(trap, a1, a2, a3 uintptr) uintptr
-.global syscall.Syscall
-syscall.Syscall:
-  movq   8(%rsp), %rax # syscall number
-  movq  16(%rsp), %rdi # arg0
-  movq  24(%rsp), %rsi # arg1
-  movq  32(%rsp), %rdx # arg2
-  syscall
-  movq %rax, 40(%rsp) # r0 uintptr
-  ret
-
