1699175bab39240d27f11e7d7010fe20f3a982ad
DQNEO <dqneoo@gmail.com>
2023-07-30 15:26:04 +0900

    Improve comments

--- src/syscall/asm_unix_amd64.s
+++ src/syscall/asm_unix_amd64.s
@@ -3,10 +3,10 @@
 // func Syscall(trap, a1, a2, a3 uintptr) uintptr
 .global syscall.Syscall
 syscall.Syscall:
-  movq   8(%rsp), %rax # syscall number
-  movq  16(%rsp), %rdi # arg0
-  movq  24(%rsp), %rsi # arg1
-  movq  32(%rsp), %rdx # arg2
+  movq   8(%rsp), %rax # trap (syscall number)
+  movq  16(%rsp), %rdi # a1
+  movq  24(%rsp), %rsi # a2
+  movq  32(%rsp), %rdx # a3
   syscall
   movq %rax, 40(%rsp) # r0 uintptr
   ret
