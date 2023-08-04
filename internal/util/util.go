54d497bb792265ce81f43da6ac1159906f07d9f3
DQNEO <dqneoo@gmail.com>
2023-08-04 22:40:59 +0900

    gc

--- internal/util/util.go
+++ internal/util/util.go
@@ -6,21 +6,7 @@ import (
 	"github.com/DQNEO/babygo/lib/fmt"
 )
 
-const ThrowFormat string = "%T"
-
-func Throw(x interface{}) {
-	panic(fmt.Sprintf(ThrowFormat, x))
-}
-
 // Logf writes General debug log to stderr
 func Logf(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, a...)
-}
-
-var CurrentPkgName string
-
-func Assert(bol bool, msg string, caller string) {
-	if !bol {
-		panic(CurrentPkgName + ":" + caller + ": " + msg)
-	}
 }
