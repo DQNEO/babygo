c5285dceab6fb7c88e869d9ca136d31142bb065b
DQNEO <dqneoo@gmail.com>
2023-08-05 16:55:40 +0900

    Cleanup Usage

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ const ProgName string = "babygo"
 func showHelp() {
 	fmt.Printf("Usage:\n")
 	fmt.Printf("    version:  show version\n")
+	fmt.Printf("    build -o <outfile> <pkgpath> e.g. build -o hello ./example/hello\n")
 	fmt.Printf("    compile -o <tmpbasename> <pkgpath> e.g. compile -o /tmp/os os\n")
-	fmt.Printf("    list -deps <pkgpath> e.g. list -deps ./t/\n")
+	fmt.Printf("    list -deps <pkgpath> e.g. list -deps ./t\n")
+	fmt.Printf("    link -o <outfile> <objfiles...>\n")
 }
 
 func showVersion() {
@@ -59,21 +61,21 @@ func main() {
 	}
 
 	switch os.Args[1] {
-	case "compile": // e.g. WORKDIR=/tmpfs/bbg/bbg-test.d babygo compile -o /tmp/os os
-		outputBaseName := os.Args[3]
-		pkgPath := os.Args[4]
-		b.BuildOne(workdir, outputBaseName, pkgPath)
-	case "list": // e.g. WORKDIR=/tmpfs/bbg/bbg-test.d babygo list -depth ./t/
-		pkgPath := os.Args[3]
-		b.ListDepth(workdir, pkgPath, os.Stdout)
-	case "link": // e.g. babygo link -o $out_file_abs a.o b.o
-		outFilePath := os.Args[3]
-		objFileNames := os.Args[4:]
-		b.Link(outFilePath, objFileNames)
-	case "build": // e.g. babygo build -o outfile ./t
+	case "build":
 		outFilePath := os.Args[3]
 		pkgPath := os.Args[4]
 		b.Build(os.Args[0], workdir, outFilePath, pkgPath)
+	case "compile":
+		outputBaseName := os.Args[3]
+		pkgPath := os.Args[4]
+		b.BuildOne(workdir, outputBaseName, pkgPath)
+	case "list":
+		pkgPath := os.Args[3]
+		b.ListDepth(workdir, pkgPath, os.Stdout)
+	case "link":
+		outFilePath := os.Args[3]
+		objFileNames := os.Args[4:]
+		b.Link(outFilePath, objFileNames)
 	default:
 		showHelp()
 		return
