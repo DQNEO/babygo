708b0bb99cd2147cc0ceccf86485900ccf5d458e
DQNEO <dqneoo@gmail.com>
2023-08-13 16:01:23 +0900

    Refactor

--- src/reflect/reflect.go
+++ src/reflect/reflect.go
@@ -6,18 +6,22 @@ type Type struct {
 	typ *rtype
 }
 
-type emptyInterface struct {
-	typ  *rtype         // dynamic type
-	data unsafe.Pointer // pointer to the actual data of the dynamic type
+type iface struct {
+	typ        *rtype         // dynamic type
+	data       unsafe.Pointer // pointer to the actual data of the dynamic type
+	methodRef1 unsafe.Pointer // a pointer to a method def e.g. os.$File.Write
+	methodRef2 unsafe.Pointer
+	methodRef3 unsafe.Pointer
+	// methodRef4 ...
 }
 
 type rtype struct {
-	id   int
+	id   int    // dtypeID
 	name string // string representation of type
 }
 
 func TypeOf(x interface{}) *Type {
-	eface := (*emptyInterface)(unsafe.Pointer(&x))
+	eface := (*iface)(unsafe.Pointer(&x))
 	return &Type{
 		typ: eface.typ,
 	}
